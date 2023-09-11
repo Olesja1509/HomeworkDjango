5941b7c2894b9924521ab1fede5a37c6f3f4cc91
Olesja1509 <olesja.janjic@gmail.com>
2023-09-11 21:06:15 +0200

    homework FBV & CBV

---
+++ blog/views.py
@@ -0,0 +1,64 @@
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from pytils.translit import slugify
+
+from blog.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    fields = ('title', 'body', 'preview', 'publication')
+    success_url = reverse_lazy('blog:list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+
+        return super().form_valid(form)
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    fields = ('title', 'body', 'preview', 'creation_date', 'publication')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_mat = form.save()
+            new_mat.slug = slugify(new_mat.title)
+            new_mat.save()
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('blog:view', args=[self.kwargs.get('pk')])
+
+
+class ArticleListView(ListView):
+    model = Article
+    extra_context = {
+        'title': 'Skystore - мы пишем'
+    }
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = super(). get_queryset(*args, **kwargs)
+        queryset = queryset.filter(publication=True)
+
+        return queryset
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+
+        return self.object
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    success_url = reverse_lazy('blog:list')
